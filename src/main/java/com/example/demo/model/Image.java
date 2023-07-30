c5bcdb703b8bdb216922f48f115971df6275f63e
danil12389 <akverrr@gmail.com>
2023-07-30 22:50:53 +0400

    добавил чтение/запись картинок

---
+++ src/main/java/com/example/demo/model/Image.java
@@ -0,0 +1,12 @@
+package com.example.demo.model;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+public class Image {
+
+    BufferedImage image = null;
+    File f = null;
+    int width = 963;    //width of the image
+    int height = 640;
+}
